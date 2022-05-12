19bab2dcc4786bd0f14601c148d8f724e249b015
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2022-05-13 00:02:01 +0200

    " -> '

--- html_reporter/__init__.py
+++ html_reporter/__init__.py
@@ -1,9 +1,8 @@
 """An HTMLTestRunner for Python which renders a nice HTML report for your ``unittest`` results."""
 
-
 from .runner import HTMLTestRunner
 from .result import stdout_redirector, stderr_redirector
 
-__author__ = "Tuan-Phong Nguyen"
-__email__ = "tuanphong94@gmail.com"
-__version__ = "0.1.1"
+__author__ = 'Tuan-Phong Nguyen'
+__email__ = 'tuanphong94@gmail.com'
+__version__ = '0.1.1'
