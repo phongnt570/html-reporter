5177c7a93c7388a907e1c595cf0d4d341202e1c2
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2022-05-13 15:15:26 +0200

    better opening stream

--- html_reporter/runner.py
+++ html_reporter/runner.py
@@ -65,7 +65,7 @@ class TestGroupReport:
 
     def total_count(self):
         return self.pass_count + self.fail_count \
-            + self.fail_count + self.skip_count
+               + self.fail_count + self.skip_count
 
 
 class HTMLTestRunner(object):
@@ -93,12 +93,10 @@ class HTMLTestRunner(object):
             if nothing is given, it uses the default template, defaults to None
         :type template: Union[str, Path], optional
         :param open_in_browser: option to open the report file in browser
-        ßafter rendering, defaults to False
-        :type open_in_browser: bool, optional
-        """
+            after rendering, defaults to False
+        :type open_in_browser: bool, optional"""
 
         self.report_filepath = Path(report_filepath)
-        self.stream = open(report_filepath, "w+")
         self.verbosity = verbosity
 
         self.title = title
@@ -118,15 +116,13 @@ class HTMLTestRunner(object):
         :param test: test case or test suite
         :type test: Union[unittest.TestCase, unittest.TestSuite]
         :return: an HTML test result
-        :rtype: HTMLTestResult
-        """
+        :rtype: HTMLTestResult"""
         try:
             result = HTMLTestResult(verbosity=self.verbosity)
             test(result)
             self.stop_time = datetime.datetime.now()
 
             self.generate_report(result)
-            self.stream.flush()
 
             sys.stderr.write(
                 f"\nTime Elapsed: {self.stop_time - self.start_time}\n")
@@ -137,12 +133,12 @@ class HTMLTestRunner(object):
                 webbrowser.open_new_tab(
                     f"file://{self.report_filepath.absolute()}")
         finally:
-            self.stream.close()
+            pass
         return result
 
     def generate_report(self, result: HTMLTestResult) -> None:
         """Render the HTML report using ``self.template`` template file.
-        The folowing variables are available to the template:
+        The following variables are available to the template:
 
         - ``title``: HTML page title
         - ``description``: description of the report
@@ -150,7 +146,7 @@ class HTMLTestRunner(object):
         - ``start_time``: start time of the test run
         - ``stop_time``: stop time of the test run
         - ``test_groups``: list of test groups
-        (type: List[:class:`TestGroupReport`])
+            (type: List[:class:`TestGroupReport`])
 
         :param result: the HTMLTestResult object
         :type result: HTMLTestResult
@@ -167,7 +163,8 @@ class HTMLTestRunner(object):
             test_groups=test_groups,
         )
 
-        self.stream.write(output)
+        with open(self.report_filepath, "w+") as fp:
+            fp.write(output)
 
     def generate_report_table(self, result: HTMLTestResult) \
             -> List[TestGroupReport]:
