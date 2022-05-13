16546a4240a913adcc7f70b285a56c6485b25843
Tuan-Phong Nguyen <tuanphong94@gmail.com>
2022-05-13 15:24:27 +0200

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/phongnt570/html-reporter',
-    version='0.2.5',
+    version='0.2.6',
     zip_safe=False,
 )
